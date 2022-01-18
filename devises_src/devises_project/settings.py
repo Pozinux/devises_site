fe1c28080b1034fb6ca06b5d7a5d26babe69c6c1
Pozinux <tim.poznanski@gmail.com>
2022-01-18 07:03:45 +0100

    Updates

--- devises_src/devises_project/settings.py
+++ devises_src/devises_project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
