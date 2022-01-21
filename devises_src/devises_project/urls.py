e326062218fb78fd8b94de61e86f2eb403f82fee
Pozinux <tim.poznanski@gmail.com>
2022-01-21 17:07:35 +0100

    Correct POST to GET info

--- devises_src/devises_project/urls.py
+++ devises_src/devises_project/urls.py
@@ -20,7 +20,7 @@ from dashboard_app.views import dashboard, redirect_index
 urlpatterns = [
     # on appelle la fonction redirect_index qui se trouve dans views.py quand on est sur la page d'accueil (donc chaine vide rajoutée à la suite de l'url)
     path("", redirect_index), 
-    # on peut passer un int et une str comme une sorte de type de POST à la fonction dashboard
+    # on peut passer un int et une str comme une sorte de GET HTTP à la fonction dashboard
     # On donne un nom à l'url (ici home) pour pouvoir l'utiliser dans le template pour retrouver le chemin complet vers l'url
     path("days=<int:days_range>&currencies=<str:currencies>", dashboard, name='home'), 
     # url qui nous permet d'accèder à l'interface d'administration quand on met /admin à la suite de l'url
